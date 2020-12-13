267cbdeefd5559311de8f325fefefc13a508455a
Kim Jangwook <kim.jangwook.1029@gmail.com>
2020-12-13 09:41:49 +0900

    blur이벤트, newTodo용도, Array.prototype.reduce의 설명

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -60,6 +60,18 @@ import Footer from './Footer'
      const { filter } = this.state
 
      const filteredTodos = todos.filter(TODO_FILTERS[filter])
+     /**
+      * Array.prototype.reduce란? 배열의 각 요소에 대해 주어진 리듀서 함수를 실행하고, 하나의 결과값을 반환
+      *   구문 => arr.reduce(callback[, initialValue])
+      * 참고 링크 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+      *
+      * 해설:
+      *   reduce의 첫번째 매개변수는 리듀서 콜백함수, 두번째 매개변수는 초기값을 의미.
+      *   밑의 코드에서
+      *     리듀서 callback 함수는 (count, todo) => { return todo.completed ? count + 1 : count }이고
+      *     초기값은 0 임.
+      *   결론만 얘기하자면 사실 두번째 매개변수 0 은 없어도 결과가 같음.
+      */
      const completedCount = todos.reduce((count, todo) => {
        return todo.completed ? count + 1 : count
      }, 0)
--- src/components/TodoTextInput.js
+++ src/components/TodoTextInput.js
@@ -26,11 +26,30 @@ export default class TodoTextInput extends Component {
 
   handleChange = e => this.setState({text: e.target.value})
 
+  /**
+   * blur란? 엘리먼트의 포커스가 해제되었을 때 발생, focusout 이벤트와는 달리 이벤트 버블링이 발생하지 않는다.
+   * 참고 링크 : https://developer.mozilla.org/ko/docs/Web/Events/blur
+   *
+   * 해설:
+   *   blur라는 이벤트를 다루는 함수로, TodoTextInput 컴포넌트의 <input> 엘리먼트의 포커스가 해제돼었을 때 발생.
+   *   이해를 위해 console.log(this.props)를 사용하여 언제 이벤트가 실행되는지 확인해 볼 것.
+   *   또한  !this.props.newTodo 조건을 비활성화 하면 어떠한 일이 발생하는지도 확인해 볼 것.
+   * @param e
+   */
   handleBlur = e => {
-    // console.log(this.props)
-    if (!this.props.newTodo) {
+    // console.log(this.props) // 이해를 위해 이 라인을 추가하여 언제 이벤트가 실행되는지 확인
+    if (!this.props.newTodo) { // 조건 비활성화를 위해 이 라인을 코멘트아웃
+
+      /**
+       * newTodo에 대한 설명
+       *   이 앱의 경우 Header.js에서 무조건 newTodo 값을 true로 설정하여 TodoTextInput.js에 전달하고 있음.
+       *   기초적인 앱이기에 TodoItem의 수정이라는 기능이 존재하지 않으므로 newTodo는 언제나 true가 됨.
+       *   만약 이미 등록된 TodoItem의 수정이라는 기능이 존재한다면 newTodo 값은 true이거나 false가 될 수 있음.
+       *   수정할 때에는 newTodo, id 라는 최소 두가지의 정보가 필요할 것으로 생각됨.
+       */
+
       this.props.onSave(e.target.value)
-    }
+    } // 조건 비활성화를 위해 이 라인을 코멘트아웃
   }
 
   render() {
